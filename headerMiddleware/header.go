c5e4962bfc29e89ff007289ff576a4da94d57874
Martin Tournoij <martin@arp242.net>
2018-01-24 14:44:57 +0000

    Move apiversionMiddleware to headerMiddleware
    This is more general/useful, and not that much more difficult to use in
    the case of the API-Version header.

---
+++ headerMiddleware/header.go
@@ -0,0 +1,19 @@
+package headerMiddleware // import "github.com/teamwork/middleware/headerMiddleware"
+
+import (
+	"net/http"
+)
+
+// Set header values, overwriting any previous value.
+func Set(h http.Header) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			for k, vals := range h {
+				for _, v := range vals {
+					w.Header().Set(k, v)
+				}
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
+}
